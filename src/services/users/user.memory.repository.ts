a441396f87fdef2044cc71eeb84fc0a668e792a0
antonrevenok <anton.revenok@informa.com>
2021-12-12 02:18:59 +0300

    Add TSDoc to user.memory.repository funcitons

--- src/services/users/user.memory.repository.ts
+++ src/services/users/user.memory.repository.ts
@@ -2,14 +2,34 @@ import { UserInterface } from './user.model';
 
 let users: UserInterface[] = [];
 
+/**
+ * Returns all users from repository
+ * @returns users array from temporary db
+ */
 const findAll = () => users;
 
+/**
+ * Returns user with provided id
+ * @param id id of the user
+ * @returns user according to the transmitted id or undefined
+ */
 const findById = (id: string) => users.find((i) => i.id === id);
 
+/**
+ * Add new user to the users repository
+ * @param user object with user fields
+ * @returns Nothing is returned.
+ */
 const addNewUser = (user: UserInterface) => {
   users.push(user);
 };
 
+/**
+ * Updates user with provided id according to provided data
+ * @param id id of the user
+ * @param data object with parameters which should be updated for current user
+ * @returns user with updated fields according to the transmitted id or null
+ */
 const updateUser = (id: string, data: UserInterface) => {
   const userIndex = users.findIndex((i) => i.id === id);
   if (userIndex !== -1) {
@@ -19,6 +39,11 @@ const updateUser = (id: string, data: UserInterface) => {
   return null;
 };
 
+/**
+ * Delete user with provided id
+ * @param id id of the user
+ * @returns user which was deleted or null
+ */
 const deleteUser = (id: string) => {
   const user = findById(id);
   if (user) {
