7b41e2cf68f537a0df2216ea7af3b17880204a93
antonrevenok <anton.revenok@informa.com>
2022-02-04 20:12:32 +0300

    Add task validation

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,13 +1,24 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
   title: string;
 
+  @IsString()
   description: string;
 
+  @IsNumber()
   order: number;
 
+  @IsString()
+  @IsOptional()
   userId?: string | null;
 
+  @IsString()
+  @IsOptional()
   boardId?: string | null;
 
+  @IsString()
+  @IsOptional()
   columnId?: string | null;
 }
--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString, IsOptional } from 'class-validator';
 import { CreateTaskDto } from './create-task.dto';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  id: string;
+  @IsString()
+  @IsOptional()
+  id?: string;
 }
